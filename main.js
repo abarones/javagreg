2d60bace4681beb055b3b8b2b62b52d68ca01ba1
abarones <146740574+abarones@users.noreply.github.com>
2023-10-16 11:47:11 -0300

    Update javagreg code

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
-}
+const treigo = document.querySelector
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom() {
+    treigo('#som_tecla_pom').play();
+}
+treigo('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap() {
+    treigo('#som_tecla_clap').play();
+}
+treigo('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomTim() {
+    treigo('#som_tecla_tim').play();
+}
+treigo('.tecla_tim').onclick = tocaSomTim;
+
+function tocaSomPuff() {
+    treigo('#som_tecla_puff').play();
+}
+treigo('.tecla_puff').onclick = tocaSomPuff;
+
+function tocaSomSplash() {
+    treigo('#som_tecla_Splash').play();
+}
+treigo('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomToim() {
+    treigo('#som_tecla_Toim').play();
+}
+treigo('.tecla_toim').onclick = tocaSomToim;
+
+function tocaSomPsh() {
+    treigo('#som_tecla_psh').play();
+}
+treigo('.tecla_psh').onclick = tocaSomPsh;
+
+function name(params) {
+    
+}function tocaSomTic() {
+    treigo('#som_tecla_tic').play();
+}
+treigo('.tecla_tic').onclick = tocaSomTic;
+
+function tocaSomTom() {
+    treigo('#som_tecla_tom').play();
+}
+treigo('.tecla_tom').onclick = tocaSomTom;
